ea739201f5502eedd983e281c8163feab998759f
Peter Hui <huishikuen@gmail.com>
2022-01-15 22:27:58 -0800

    added Grid in Items page and added functionalities of search bar

--- frontend/src/Items.js
+++ frontend/src/Items.js
@@ -1,41 +1,55 @@
 import { Container } from '@mui/material'
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 import Item from './components/Item'
 import SearchBar from './components/SearchBar'
+import Grid from '@mui/material/Grid';
+
 function Items() {
   const data = [
-    {id: 1,
-    description: 'cat black cute nice 123',
-    day: '2021-01-15 17:38',
-    location: 'BCIT SE06 320',
-    contacts: '236-990-3350',
-    img: 'https://nwhacks2022storage.blob.core.windows.net/image/sample-cat-0.jpeg'
-  }, 
-  {id: 2,
-    description: 'dog yellow brown hairy fun',
-    day: '2021-01-13 19:38',
-    location: 'Central Park',
-    contacts: '236-996-1017',
-    img: 'https://picsum.photos/200/300'},
-     {id: 3,
+    {
+      id: 1,
+      description: 'cat black cute nice 123',
+      day: '2021-01-15 17:38',
+      location: 'BCIT SE06 320',
+      contacts: '236-990-3350',
+      img: 'https://nwhacks2022storage.blob.core.windows.net/image/sample-cat-0.jpeg'
+    },
+    {
+      id: 2,
+      description: 'dog yellow brown hairy fun',
+      day: '2021-01-13 19:38',
+      location: 'Central Park',
+      contacts: '236-996-1017',
+      img: 'https://picsum.photos/200/300'
+    },
+    {
+      id: 3,
       description: 'phone iphone black new',
       day: '2021-01-13 19:38',
       location: 'Expo Line MetroTown station',
       contacts: 'peterpang1103@gmail.com',
-      img: 'https://picsum.photos/200/299'}
+      img: 'https://picsum.photos/200/299'
+    }
   ]
 
 
 
   return (
     <Fragment>
-      <Container maxWidth='xs' sx={{mb:10}}>
-      <SearchBar />
-    {data.map((item) => 
-      (<Item key={item.id} item={item} />)
-    )}
-    </Container>
-    </ Fragment>
+      <div style={{marginLeft: "auto", marginRight: "auto", marginTop:"20px"}}>
+        <SearchBar />
+      </div>
+      <Grid container spacing={2}>
+        {data.map((item) =>
+        (
+          <Grid item xs={12} sm={6} md={4} lg={3}>
+            <Item key={item.id} item={item} />
+          </Grid>
+        )
+        )}
+      </Grid>
+    </ Fragment >
+
   )
 }
 
--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,20 +1,39 @@
-import {React, Fragment} from 'react'
+import { React, Fragment } from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import { TextField } from '@mui/material';
+import { useState } from 'react';
 
 function SearchBar() {
+  const [isFocused, setFocus] = useState(false);
 
+
+  const handleEnterKeyPress = (e) => {
+    if (e.keyCode == 13) {
+      sendPostRequestToAPI(e.target.value);
+    }
+  }
+
+  const handleClickOfSearchIcon = () => {
+    const userInput = document.getElementById("standard-basic").value;
+    sendPostRequestToAPI(userInput);
+  }
 
   return (
     <Fragment>
-    <TextField id="standard-basic" label="Search" variant="standard" sx={{width: 300}}
-    InputProps={{
-      endAdornment: (
-       <SearchIcon / > 
-      )
-    }}/>
+      <TextField id="standard-basic" label="Search" variant="standard" sx={{ width: 360 }}
+        InputProps={{
+          endAdornment: (
+            <SearchIcon onClick={handleClickOfSearchIcon} />
+          )
+        }}
+        onKeyDown={handleEnterKeyPress} />
     </Fragment>
   )
 }
 
-export default SearchBar
+function sendPostRequestToAPI(text){  
+  console.log(text);
+  document.getElementById("standard-basic").value = "";
+}
+
+export default SearchBar;
