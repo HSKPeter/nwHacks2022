b07021d0177ccaa2ef68a223ae871c390cf7230b
pangchunh <cpang26@my.bcit.ca>
2022-01-15 17:57:46 -0800

    added items and card for each item, now testing with fake data.

--- frontend/src/Items.js
+++ frontend/src/Items.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import * as React from 'react';
+import Item from './components/Item'
 
 function Items() {
+  const data = [
+    {id: 1,
+    description: 'cat black cute nice 123',
+    day: '2021-01-15 17:38',
+    location: 'BCIT SE06 320',
+    contacts: '236-990-3350',
+    img: 'https://nwhacks2022storage.blob.core.windows.net/image/sample-cat-0.jpeg'
+  }, 
+  {id: 2,
+    description: 'dog yellow brown hairy fun',
+    day: '2021-01-13 19:38',
+    location: 'Central Park',
+    contacts: '236-990-3350',
+    img: 'https://nwhacks2022storage.blob.core.windows.net/image/sample-cat-0.jpeg'},
+     {id: 3,
+      description: 'phone iphone black new',
+      day: '2021-01-13 19:38',
+      location: 'Expo Line MetroTown station',
+      contacts: '236-990-3350',
+      img: 'https://nwhacks2022storage.blob.core.windows.net/image/sample-cat-0.jpeg'}
+  ]
+
+
+
   return (
-    <div>
-      This is Items.js.
-    </div>
+    <>
+    {data.map((item) => 
+      (<Item key={item.id} item={item} />)
+    )}
+    </>
   )
 }
 
